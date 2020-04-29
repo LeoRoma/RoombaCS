b662e8e9496af2c4277b91c3543b87e4c59157b5
LeoRoma <xiajtn@gmail.com>
2020-04-29 12:20:59 +0100

    RoomTests implemented with CheckPointIsOnRoom and CheckPointIsNotOnRoom

--- RoombaCSTests/RoomTests.cs
+++ RoombaCSTests/RoomTests.cs
@@ -12,12 +12,34 @@ namespace RoombaCS.Tests
         public void RoomTest()
         {
             int width = 5;
-            int height = 6;
+            int height = 5;
 
             var room = new Room(width, height);
 
             Assert.Equal(width, room.Width);
             Assert.Equal(height, room.Height);
+        }
+
+        [Fact]
+        public void CheckPointIsOnRoom()
+        {
+            var room = new Room(5, 5);
+            var point = new Point(2, 3);
+
+            var expected = true;
+
+            Assert.Equal(expected, room.OnRoom(point));
+        }
+
+        [Fact]
+        public void CheckPointIsNotOnRoom()
+        {
+            var room = new Room(5, 5);
+            var point = new Point(6, 6);
+
+            var expected = false;
+
+            Assert.Equal(expected, room.OnRoom(point));
         }
     }
 }
